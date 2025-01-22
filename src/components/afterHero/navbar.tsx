9532cdd3a30252c20e099eb1c7aaef9c715d14e2
RolandVrignon <roland.vrignon@gmail.com>
2025-01-22 15:34:51 +0100

    wip

--- src/components/afterHero/navbar.tsx
+++ src/components/afterHero/navbar.tsx
@@ -6,16 +6,16 @@ export const Navbar = ({ isVisible }: { isVisible: boolean }) => {
             className={`fixed z-50 top-0 w-full transition-opacity duration-300 ${isVisible ? "opacity-100" : "opacity-0 pointer-events-none"}`}
         >
             <Link href="/" className="relative w-full flex justify-center items-center">
-                    <span className="absolute jacquard-24-regular blur-sm opacity-50 text-6xl text-[#ff0000] w-full text-center">
+                    <span className="absolute jacquard-24-regular blur-sm opacity-50 text-4xl md:text-6xl text-[#ff0000] w-full text-center">
                         &gt; ./rolandv.sh_
                     </span>
 
-                    <span className="absolute jacquard-24-regular blur-sm opacity-50 text-6xl text-[#ff0000] w-full text-center">
+                    <span className="absolute jacquard-24-regular blur-sm opacity-50 text-4xl md:text-6xl text-[#ff0000] w-full text-center">
                         &gt; ./rolandv.sh_
                     </span>
 
                     {/* Texte principal */}
-                    <span className="jacquard-24-regular  text-6xl text-[#ff0000] w-full text-center">
+                    <span className="jacquard-24-regular  text-4xl md:text-6xl text-[#ff0000] w-full text-center">
                         &gt; ./rolandv.sh_
                     </span>
             </Link>
